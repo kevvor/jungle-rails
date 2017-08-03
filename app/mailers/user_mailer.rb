ce67b742dea08066784e8a054366324167abf02b
keano voelkl <keanov@gmail.com>
2017-08-03 18:50:25 +0000

    email works

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
 
-  def order_confirmation_email(user)
-    @user = user
-    @url = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Your order has been processed :)')
+  def order_confirmation_email(order)
+    @order = order
+    @email = @order.email
+    @total = @order.total_cents
+    mail(to: @email, subject: "order number #{@order.id} :)")
   end
 end
