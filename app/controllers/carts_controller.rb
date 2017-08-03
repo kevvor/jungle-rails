daf25ef81f990f8088c73993bb5f1f4b518f30ea
keano voelkl <keanov@gmail.com>
2017-08-03 20:51:38 +0000

    starting new feature

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,10 @@
 class CartsController < ApplicationController
 
   def show
+    if cart == {}
+      flash[:notice] = "Post successfully created"
+      puts 'notice'
+    end
   end
 
   def add_item
