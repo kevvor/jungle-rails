57e7eaed4eea3d80a74fb7b7930a3ca915e19af7
keano voelkl <keanov@gmail.com>
2017-08-03 15:51:16 +0000

    feature done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+    puts 'inside current_user'
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  # def cool_method
+  #   puts "i'm awesome"
+    # return false
+  # end
+
+
   private
 
   def cart
