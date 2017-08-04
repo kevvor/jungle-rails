be576307caac1ede123ca0fd67833d4ba8e991fe
keano voelkl <keanov@gmail.com>
2017-08-04 14:21:58 +0000

    reviews saved and display to correct product and user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,26 @@
 class ReviewsController < ApplicationController
   def create
-    @review = Review.new(params[:description])
-    @review.user_id = session[:user_id]
-    @review.save
-    redirect_to :products
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.product_id = params[:product_id]
+
+    if @review.save
+      puts 'this is the saved review'
+      puts @review.inspect
+      redirect_to :back
+    else
+      redirect_to :back
+    end
+
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
+  end
+
 end
